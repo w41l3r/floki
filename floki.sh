7d4d5ca7f474e966a3a1eaf7dd9556d60c4f03d4
w41l3r <66040315+w41l3r@users.noreply.github.com>
2024-01-04 17:38:05 -0300

    Update floki.sh

--- floki.sh
+++ floki.sh
@@ -12,9 +12,7 @@ if [ $# -ne 1 ];then
 fi
 
 #
-# To do's:
-#	- function to check/install pre-reqs
-#	- some ffuf recon
+# To do - function to check/install pre-reqs
 #
 # Pre-reqs:
 # 	- amass
@@ -31,6 +29,7 @@ fi
 #	- assetfinder's DNS wordlist
 #
 #!!manually configure subfinder's provider-config.yaml (API Keys)
+#!!!remember to configure sudoers to NOPASSWD to run nmap
 
 DOMAIN=$1
 WORDLISTS="/opt/tools/wordlists"
@@ -42,17 +41,17 @@ DNSWORDLIST="${WORDLISTS}/best-dns-wordlist.txt"
 
 mkdir ${DOMAIN}
 if [ $? -ne 0 ];then
-	echo "\n\\033[31m[!] Failed to create output directory. Bye!\\033[0m"
+	print "\n\\033[31m[!] Failed to create output directory. Bye!\\033[0m"
 	exit 1
 fi
 
 cd ${DOMAIN}
 
-echo "\n\\033[33m[*] Starting Amass...\\033[0m"
+print "\n\\033[33m[*] Starting Amass...\\033[0m"
 amass enum -passive -norecursive -noalts -d ${DOMAIN} -o amass.txt
-echo "\n\\033[33m[*] Starting Assetfinder...\\033[0m"
+print "\n\\033[33m[*] Starting Assetfinder...\\033[0m"
 assetfinder -subs-only ${DOMAIN} | tee assetfinder.txt
-echo "\n\\033[33m[*] Starting Subfinder...\\033[0m"
+print "\n\\033[33m[*] Starting Subfinder...\\033[0m"
 subfinder -d ${DOMAIN} -silent -pc /etc/subfinder/provider-config.yaml |tee subfinder.txt
 
 cat amass.txt assetfinder.txt subfinder.txt | sort -u > subs.txt
@@ -60,12 +59,19 @@ cat amass.txt assetfinder.txt subfinder.txt | sort -u > subs.txt
 #
 #!!! Starting some active recon here...
 #
+FIERCEFLAG=1
 if [ ! -s $DNSWORDLIST ];then
-	echo "$DNSWORDLIST not found!"
-	exit 1
-else
-	echo "\n\\033[33m[*] Starting Fierce...\\033[0m"
-	echo "\\033[33m Relax...you really should go get some coffee...\\033[0m"
+	print "\n\\033[31m$DNSWORDLIST empty or inexistent!"
+	print "Trying to get it...\\033[0m"
+	wget https://wordlists-cdn.assetnote.io/data/manual/best-dns-wordlist.txt -O $DNSWORDLIST
+	if [ $? -ne 0 ];then
+		print "\n\\033[31mError downloading the DNS wordlist. Wont use fierce!\\033[0m"
+		FIERCEFLAG=0
+	fi
+fi
+if [ $FIERCEFLAG -eq 1 ];then
+	print "\n\\033[33m[*] Starting Fierce...\\033[0m"
+	print "\\033[33m Relax...you really should go get some coffee...\\033[0m"
 	fierce --domain $DOMAIN --traverse 3 --subdomain-file $DNSWORDLIST > fierce.txt
 	cat fierce.txt
 	if grep "Whoah, it worked" fierce.txt
@@ -77,41 +83,41 @@ else
 		mv allsubs.txt subs.txt
 	fi	
 fi
+
 #finished active recon (fierce)
 
 if [ -s subs.txt ];then
-	echo "\n\\033[33m[*] Starting waybackurls...\\033[0m"
+	print "\n\\033[33m[*] Starting waybackurls...\\033[0m"
 	cat subs.txt | waybackurls | tee waybackurls.txt
 	if [ ! -s waybackurls.txt ];then
-		echo "\n\\033[31m[!] No entries found on waybackurls!\\033[0m"
+		print "\n\\033[31m[!] No entries found on waybackurls!\\033[0m"
 		continue
 	fi
-	echo "\n\\033[33m[*] Starting httpx...\\033[0m"
+	print "\n\\033[33m[*] Starting httpx...\\033[0m"
 	cat waybackurls.txt |grep -v "^$"| httpx --follow-redirects -mc 200,302 | tee httpx.txt
 	if [ -s httpx.txt ];then
 		cat httpx.txt | cut -f1,2,3 -d'/' | sort -u > unique-httpx.txt
-		echo "\n\\033[33m[*] Starting whatweb...\\033[0m"
+		print "\n\\033[33m[*] Starting whatweb...\\033[0m"
 		whatweb -U=Googlebot -i unique-httpx.txt| tee whatweb.txt
 		cat httpx.txt | cut -f3 -d'/' |sort -u > unique-http-subs.txt
-		echo "\n\\033[33m[*] Starting gowitness...\\033[0m"
+		print "\n\\033[33m[*] Starting gowitness...\\033[0m"
 		gowitness file -f unique-http-subs.txt
 		echo
-		echo "\n\\033[33m[!] Finished Gowitness gathering... run 'gowitness server' on $PWD after floki to see results!!\\033[0m"
+		print "\n\\033[33m[!] Finished Gowitness gathering... run 'gowitness server' on $PWD after floki to see results!!\\033[0m"
 		echo
 	fi
-	echo "\n\\033[33m[*] Starting nuclei...\\033[0m"
-	echo "\\033[33m This is REALLY going to take some time... pls be patient!"
+	print "\n\\033[33m[*] Starting nuclei...\\033[0m"
+	print "\\033[33m This is REALLY going to take some time... pls be patient!"
 	echo
 	cat unique-httpx.txt |nuclei -t ~/.local/nuclei-templates/ -fhr |tee nuclei.txt
 	echo
-	echo "\n\\033[33m[*] Starting nmap...\\033[0m"
+	print "\n\\033[33m[*] Starting nmap...\\033[0m"
 	echo
 	sudo nmap -n -v -Pn -sS -p- --open -oA ${DOMAIN} -iL subs.txt
 else
 	echo
-	echo "\n\\033[31m[!]No subdomains found!\\033[0m"
+	print "\n\\033[31m[!]No subdomains found!\\033[0m"
 	exit 0
 fi
 
 exit 0
-                                   
