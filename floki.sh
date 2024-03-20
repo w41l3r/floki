eca991b6c6c319dc913782fc5e2597ea86d8fdb5
w41l3r <66040315+w41l3r@users.noreply.github.com>
2024-03-20 11:40:40 -0300

    Update floki.sh v0.9

--- floki.sh
+++ floki.sh
@@ -2,27 +2,11 @@
 #
 # floki.sh - Viking recon tool
 #
-# v0.7 - 14/01/2024
+# v0.9 - 20/03/2024
 #
 # W41L3R
 #
 # To do - function to check/install pre-reqs
-#
-# Pre-reqs:
-# 	- amass [https://github.com/owasp-amass/amass]
-#	- assetfinder [https://github.com/tomnomnom/assetfinder]
-#	- subfinder [https://github.com/projectdiscovery/subfinder]
-#	- httpx [https://github.com/projectdiscovery/httpx]
-#	- whatweb [https://github.com/urbanadventurer/WhatWeb]
-#	- waybackurls [https://github.com/tomnomnom/waybackurls]
-#	- knockpy [https://github.com/guelfoweb/knock] 
-#	- nuclei [https://github.com/projectdiscovery/nuclei]
-#	- mantra [https://github.com/MrEmpy/mantra]
-#	- nmap [https://nmap.org/download]
-#	- gowitness(*google-chrome is necessary) [https://github.com/sensepost/gowitness]
-#	- puredns [https://github.com/d3mondev/puredns]
-#	- assetnote's DNS wordlist [https://wordlists-cdn.assetnote.io/data/manual/best-dns-wordlist.txt]
-#	- massdns [https://github.com/blechschmidt/massdns]
 #
 # !!manually configure subfinder's provider-config.yaml (API Keys)
 # !!!remember to configure sudoers to NOPASSWD to run nmap without asking password
