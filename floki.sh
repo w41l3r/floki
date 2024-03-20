f11015cb0edfa2c55614505e7c33127bb83ddbbc
w41l3r <66040315+w41l3r@users.noreply.github.com>
2024-03-20 11:50:07 -0300

    Update floki.sh v.09

--- floki.sh
+++ floki.sh
@@ -2,23 +2,31 @@
 #
 # floki.sh - Viking recon tool
 #
-# v0.9 - 20/03/2024
+# v0.9 03/2024
 #
 # W41L3R
 #
 # To do - function to check/install pre-reqs
 #
 # !!manually configure subfinder's provider-config.yaml (API Keys)
-# !!!remember to configure sudoers to NOPASSWD to run nmap without asking password
 
 #Your wordlists directory
 WORDLISTS="/opt/tools/wordlists"
+#
 #Assetnote DNS wordlist https://wordlists-cdn.assetnote.io/data/manual/best-dns-wordlist.txt
 DNSWORDLIST="${WORDLISTS}/best-dns-wordlist.txt"
+#
 #Nuclei templates directory
 NUCLEIDIR="/home/w41l3r/.local/nuclei-templates" 
+#
 #Wordlist to be used with ffuf (coming soon...)
 #WEBWORDLIST="${WORDLISTS}/common.txt" - will be necessary soon..
+#
+#Resolvers file to use with puredns
+RESOLVERS="/home/w41l3r/.config/puredns/resolvers.txt"
+#
+#Burp proxy listener to feed (send requests to httpx results)
+BURPROXY="http://127.0.0.1:8080"
 
 #
 # dont change anything after here...
@@ -27,9 +35,9 @@ DOMAIN=$1
 
 cat `dirname $0`/banner.txt
 
-if [ $# -ne 1 ];then
+if [ $# -ne 1 -a $# -ne 2 ];then
  echo
- echo "Syntax: $0 domain_name"
+ echo "Syntax: $0 domain_name [-b] [-h]"
  exit 9
 fi
 
@@ -37,27 +45,28 @@ function printHelp {
 	echo
  	echo "Syntax: $0 [-h] [-b] domain.com"
   	echo " -h: Shows this help"
-   	echo " -b: Brute force DNS (requires a dns wordlist. Pls check DNSWORDLIST variable!)"
+   	echo " -b: Brute force DNS (requires a dns wordlist and a resolvers file. Pls check the DNSWORDLIST and RESOLVERS variables!)"
+    	echo "     How to get a resolvers file: dnsvalidator -tL https://public-dns.info/nameservers.txt -threads 20 -o resolvers.txt"
     	echo
      	exit 9
 }
 
-OPTSTRING=":hb"
+BRUTEDNS=0
 
-while getopts ${OPTSTRING} opt; do
-  case ${opt} in
-    h)
+if [ $# -eq 2 ];then
+case $2 in
+    "-h")
       printHelp
       ;;
-    b)
+    "-b")
       BRUTEDNS=1
       ;;
-    ?)
-      echo "Invalid option: -${OPTARG}."
+    *)
+      echo "Invalid option: $2 "
       exit 1
       ;;
-  esac
-done
+esac
+fi
 
 #check dependencies/binaries
 DEPENDENCIES="amass assetfinder subfinder puredns mantra waybackurls httpx whatweb gowitness nmap nuclei knockpy gau katana uro"
@@ -100,7 +109,6 @@ fi
 cd ${DOMAIN}
 
 ZONEXFER=0
-BRUTEDNS=0
 
 echo -e "\n\\033[33m[*] Trying Zone Xfer... \\033[0m"
 
@@ -150,11 +158,15 @@ else    #zonetransfer didnt work... lets do some brute force
 			echo -e "\n\\033[31mError downloading the DNS wordlist. Bye!\\033[0m"
 			exit 1
 	 	fi
-        else #we have everything... lets brute DNS
-		
+        else
+		if [ ! -s $RESOLVERS ];then
+  			echo -e "\n\\033[31mResolvers file empty or not found. Please check the RESOLVERS variable.\\033[0m"
+     			echo -e "\n\\033[31mI recommend using dnsvalidator to generate an updated resolvers.txt file.\\033[0m"
+     			exit 1
+		fi
 		echo -e "\n\\033[33m[*] Starting Puredns...\\033[0m"
 		echo -e "\\033[33m Relax...you really should go get some coffee...\\033[0m"
-		puredns bruteforce $DNSWORDLIST $DOMAIN -q > puredns.txt
+		puredns bruteforce $DNSWORDLIST $DOMAIN --resolvers $RESOLVERS -t 10 -q > puredns.txt
   		if [ $? -ne 0 ];then
     			echo -e "\n\\033[31mError executing the DNS brute.\\033[0m"
        			echo -e "\n\\033[31mPlease check if massdns is installed and ~/.config/puredns/resolvers.txt is ok.\\033[0m"
@@ -217,11 +229,32 @@ if [ -s subs.txt ];then #successfully gathered subdomains
  	echo -e "\n\\033[33m[*] Starting nuclei...\\033[0m"
         echo -e "\\033[33m This is REALLY going to take some time... pls be patient!"
 	echo
-	cat unique-httpx.txt |nuclei -t $NUCLEIDIR -fhr |tee nuclei.txt
+	cat final-endpoints.txt |nuclei -t $NUCLEIDIR -fhr |tee nuclei.txt
 
+ 	#Send requests to Burp
+  	echo -e "\n\\033[33m[*] Im going to feed your Burp. Please check if $BURPROXY is listening...\\033[0m"
+   	echo -n -e "\n\\033[33m[*] May i start sending requests to burp(Y|n)? \\033[0m"
+    	read RESP
+     	case $RESP in
+        y|Y)
+		curl -s -v -k $BURPROXY | grep -i burp >/dev/null
+  		if [ $? -eq 0 ];then
+    			cat httpx.txt | awk '{print $1}' | while read linha 
+			do                 
+				curl --proxy $BURPROXY -s -k $linha
+			done
+   		else
+     			echo -e "\n\\033[31mError comunicating with BURP.\\033[0m"
+			
+    		fi
+      
+	;;
+ 	*)
+		echo "ok, no Burp then..."
+  	;;
+   	esac
  	#Run Nmap
- 	#echo
-      	#echo -e "\n\\033[33m[*] Starting nmap...\\033[0m"
+ 	#echo -e "\n\\033[33m[*] Starting nmap...\\033[0m"
 	#echo
 	#sudo nmap -n -v -Pn -sS -p- --open -oA ${DOMAIN} -iL subs.txt
  
